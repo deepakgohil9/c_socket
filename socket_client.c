9a69e4d9ef5e2eb9931397f79fad7fb2ccaf7d74
Gohil Deepak <gohildeepak2003@gmail.com>
2023-08-05 22:21:15 +0530

    Update socket_client.c

--- socket_client.c
+++ socket_client.c
@@ -12,46 +12,48 @@
 int main()
 {
 
-    // declaring and initialization of variables if required
-    int sock, connection_status;
-    int recv_len = 0, total_recv_len = 0;
-    struct sockaddr_in server_address;
-    char *server_ip = "142.251.33.68";
-    char request[REQUEST_LEN] = "GET / HTTP/1.0\r\n\r\n";
-    char server_response[RESPONSE_LEN];
+	// declaring and initialization of variables if required
+	int sock, connection_status;
+	int recv_len = 0, total_recv_len = 0;
+	struct sockaddr_in server_address;
+	char *server_ip = "54.204.94.184";
+	char request[REQUEST_LEN] = "GET / HTTP/1.0\r\n\r\n";
+	char server_response[RESPONSE_LEN + 1];
+	server_response[1024] = '\0'; // null character at the end of buffer so that output will not get overlapped
 
-    // creating a socket
-    sock = socket(AF_INET, SOCK_STREAM, 0);
+	// creating a socket
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    // checking socket created or not
-    if (sock == -1)
-        printf("[ERROR] unable to create socket");
+	// checking socket created or not
+	if (sock == -1)
+		printf("[ERROR] unable to create socket");
 
-    // setting address of server to connect socket
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(80);
-    server_address.sin_addr.s_addr = inet_addr(server_ip);
+	// setting address of server to connect socket
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(80);
+	server_address.sin_addr.s_addr = inet_addr(server_ip);
 
-    // connecting socket
-    connection_status = connect(sock, (struct sockaddr *)&server_address, sizeof(server_address));
+	// connecting socket
+	connection_status = connect(sock, (struct sockaddr *)&server_address, sizeof(server_address));
 
-    // checking connection status
-    if (connection_status == -1)
-        printf("[ERROR] connection failed");
+	// checking connection status
+	if (connection_status == -1)
+		printf("[ERROR] connection failed");
 
-    // sending request to server
-    send(sock, request, REQUEST_LEN, 0);
-    printf("[request sended to server]\n\n");
+	// sending request to server
+	send(sock, request, REQUEST_LEN, 0);
+	printf("[request sended to server]\n\n");
 
-    // receving response from server
-    while ((recv_len = recv(sock, server_response, RESPONSE_LEN, 0) > 0))
-    {
-        printf("%s", server_response);
-        total_recv_len += recv_len;
-    }
+	// receving response from server
+	while ((recv_len = recv(sock, server_response, RESPONSE_LEN, 0) > 0))
+	{
+		printf("%s", server_response);
+		memset(server_response, '\0', RESPONSE_LEN);
+		total_recv_len += recv_len;
+	}
 
-    // closing socket
-    close(sock);
+	// closing socket
+	close(sock);
 
-    return 0;
+	return 0;
 }
